a6abfc72a679c4c6cce0b9dff6ae12c271b4717b
Brad Moylan <bmoylan@palantir.com>
2018-12-14 02:22:32 -0800

    Add support for index expressions

---
+++ expression/index.go
@@ -0,0 +1,31 @@
+package expression
+
+import (
+	"go/ast"
+
+	"github.com/palantir/goastwriter/astgen"
+)
+
+// Index represents accessing an array or map by index, e.g. `myMap["myKey"]` or `myArr[0]`
+type Index struct {
+	Receiver astgen.ASTExpr
+	Index    astgen.ASTExpr
+}
+
+func NewIndex(receiver astgen.ASTExpr, index astgen.ASTExpr) *Index {
+	return &Index{
+		Receiver: receiver,
+		Index:    index,
+	}
+}
+
+func (i *Index) ASTExpr() ast.Expr {
+	var expr ast.IndexExpr
+	if i.Receiver != nil {
+		expr.X = i.Receiver.ASTExpr()
+	}
+	if i.Index != nil {
+		expr.Index = i.Index.ASTExpr()
+	}
+	return &expr
+}
